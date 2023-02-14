c54983d7d1a6865470397f0576cc6c88d48f25b1
woodbyte <pedro.s.madeira@gmail.com>
2023-02-14 21:56:38 +0000

    Add project files.

---
+++ Settings.cs
@@ -0,0 +1,58 @@
+﻿using MCM.Abstractions.Attributes;
+using MCM.Abstractions.Attributes.v2;
+using MCM.Abstractions.Base.Global;
+
+namespace Bannerlord.MoreFrameLimiters
+{
+    internal sealed class Settings : AttributeGlobalSettings<Settings>
+    {
+        public override string Id => "MoreFrameLimiters";
+        public override string FolderName => "MoreFrameLimiters";
+        public override string DisplayName => $"More Frame Limiters {typeof(Settings).Assembly.GetName().Version.ToString(3)}";
+        public override string FormatType => "json";
+
+        [SettingPropertyInteger("Frame Limiter", 30, 360, RequireRestart = false, Order = 1)]
+        [SettingPropertyGroup("Default", GroupOrder = 1)]
+        public int DefaultFrameLimiter { get; set; } = 60;
+
+        [SettingPropertyBool("Map Toggle", RequireRestart = false, Order = 1, IsToggle = true)]
+        [SettingPropertyGroup("Campaign Map", GroupOrder = 2)]
+        public bool MapToggle { get; set; } = true;
+
+        [SettingPropertyInteger("Frame Limiter", 30, 360, RequireRestart = false, Order = 2)]
+        [SettingPropertyGroup("Campaign Map", GroupOrder = 2)]
+        public int MapFrameLimiter { get; set; } = 90;
+
+        [SettingPropertyBool("Siege Toggle", RequireRestart = false, Order = 1, IsToggle = true)]
+        [SettingPropertyGroup("Siege Battles", GroupOrder = 3)]
+        public bool SiegeToggle { get; set; } = false;
+
+        [SettingPropertyInteger("Frame Limiter", 30, 360, RequireRestart = false, Order = 2)]
+        [SettingPropertyGroup("Siege Battles", GroupOrder = 3)]
+        public int SiegeLimiter { get; set; } = 60;
+
+        [SettingPropertyBool("Arena Toggle", RequireRestart = false, Order = 1, IsToggle = true)]
+        [SettingPropertyGroup("Arena Fights", GroupOrder = 4)]
+        public bool ArenaToggle { get; set; } = true;
+
+        [SettingPropertyInteger("Frame Limiter", 30, 360, RequireRestart = false, Order = 2)]
+        [SettingPropertyGroup("Arena Fights", GroupOrder = 4)]
+        public int ArenaLimiter { get; set; } = 120;
+
+        [SettingPropertyBool("Visit Toggle", RequireRestart = false, Order = 1, IsToggle = true)]
+        [SettingPropertyGroup("Towns, Castles and Villages", GroupOrder = 5)]
+        public bool VisitToggle { get; set; } = false;
+
+        [SettingPropertyInteger("Frame Limiter", 30, 360, RequireRestart = false, Order = 2)]
+        [SettingPropertyGroup("Towns, Castles and Villages", GroupOrder = 5)]
+        public int VisitLimiter { get; set; } = 60;
+
+        [SettingPropertyBool("Indoor Toggle", RequireRestart = false, Order = 1, IsToggle = true)]
+        [SettingPropertyGroup("Indoor Locations", GroupOrder = 6)]
+        public bool IndoorToggle { get; set; } = true;
+
+        [SettingPropertyInteger("Frame Limiter", 30, 360, RequireRestart = false, Order = 2)]
+        [SettingPropertyGroup("Indoor Locations", GroupOrder = 6)]
+        public int IndoorLimiter { get; set; } = 120;
+    }
+}
---
+++ SubModule.cs
@@ -0,0 +1,77 @@
+﻿using TaleWorlds.CampaignSystem.GameState;
+using TaleWorlds.Core;
+using TaleWorlds.Engine.Options;
+using TaleWorlds.MountAndBlade;
+
+namespace Bannerlord.MoreFrameLimiters
+{
+    public class SubModule : MBSubModuleBase
+    {
+        GameState? _activeGameState = null;
+        int? _newFrameLimiter = null;
+
+        void ConfigureFrameLimiter(GameState activeState)
+        {
+            if (Settings.Instance == null) return;
+
+            if (activeState is InitialState)
+                _newFrameLimiter = Settings.Instance.DefaultFrameLimiter;
+            else if (activeState is MapState)
+                _newFrameLimiter = Settings.Instance.MapToggle ? Settings.Instance.MapFrameLimiter : Settings.Instance.DefaultFrameLimiter;
+            else if (activeState is MissionState missionState)
+            {
+                if (missionState.CurrentMission == null) return;
+
+                //"TournamentFight" "ArenaPracticeFight" "ArenaDuelMission"
+                //"SiegeMissionWithDeployment"
+                //"TownCenter"
+                //"Village"
+                //"Indoor"
+
+                if (missionState.MissionName.Contains("Tournament") || missionState.MissionName.Contains("Arena"))
+                    _newFrameLimiter = Settings.Instance.ArenaToggle ? Settings.Instance.ArenaLimiter : Settings.Instance.DefaultFrameLimiter;
+                else if (missionState.MissionName.Contains("Siege"))
+                    _newFrameLimiter = Settings.Instance.SiegeToggle ? Settings.Instance.SiegeLimiter : Settings.Instance.DefaultFrameLimiter;
+                else if (missionState.MissionName == "TownCenter" || missionState.MissionName == "Village")
+                    _newFrameLimiter = Settings.Instance.VisitToggle ? Settings.Instance.VisitLimiter : Settings.Instance.DefaultFrameLimiter;
+                else if (missionState.MissionName == "Indoor")
+                    _newFrameLimiter = Settings.Instance.IndoorToggle ? Settings.Instance.IndoorLimiter : Settings.Instance.DefaultFrameLimiter;
+                else
+                    _newFrameLimiter = Settings.Instance.DefaultFrameLimiter;
+            }
+        }
+
+        protected override void OnApplicationTick(float dt)
+        {
+            if (_newFrameLimiter != null)
+            {
+                if (_newFrameLimiter.Value != NativeOptions.GetConfig(NativeOptions.NativeOptionsType.FrameLimiter))
+                    NativeOptions.SetConfig(NativeOptions.NativeOptionsType.FrameLimiter, _newFrameLimiter.Value);
+
+                _newFrameLimiter = null;
+            }
+
+            if (GameStateManager.Current == null) return;
+            if (GameStateManager.Current.ActiveState != _activeGameState)
+            {
+                _activeGameState = GameStateManager.Current.ActiveState;
+
+                ConfigureFrameLimiter(_activeGameState);
+            }
+        }
+
+        public override void OnInitialState()
+        {
+            if (Settings.Instance == null) return;
+
+            Settings.Instance.PropertyChanged += Instance_PropertyChanged;
+        }
+
+        private void Instance_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (_activeGameState == null) return;
+
+            ConfigureFrameLimiter(_activeGameState);
+        }
+    }
+}
